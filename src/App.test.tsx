495819b9ae3792d02f330f67444a9c10fa3c41a4
Derek <derek@morphdev.co>
2022-11-02 19:07:22 -0500

    Updated test

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,9 @@
 import React from 'react';
 import { render, screen } from '@testing-library/react';
-import App from './App';
+import SymplerChat from './index';
 
 test('renders learn react link', () => {
-  render(<App />);
+  render(<SymplerChat urlParam='' />);
   const linkElement = screen.getByText(/learn react/i);
   expect(linkElement).toBeInTheDocument();
 });
