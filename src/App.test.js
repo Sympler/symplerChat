faed7908881616ecbb4c81fd2249c245f8899f01
Derek <derek@morphdev.co>
2022-11-11 14:37:11 -0600

    Fixed All the files

--- src/App.test.js
+++
@@ -1,8 +0,0 @@
-import { jsx as _jsx } from "react/jsx-runtime";
-import { render, screen } from '@testing-library/react';
-import SymplerChat from './index';
-test('renders learn react link', function () {
-    render(_jsx(SymplerChat, { endpoint: '', formName: '' }));
-    var linkElement = screen.getByText(/learn react/i);
-    expect(linkElement).toBeInTheDocument();
-});
--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,9 @@
 import React from 'react';
 import { render, screen } from '@testing-library/react';
-import SymplerChat from './index';
+import App from './App';
 
 test('renders learn react link', () => {
-  render(<SymplerChat endpoint='' formName=''/>);
+  render(<App />);
   const linkElement = screen.getByText(/learn react/i);
   expect(linkElement).toBeInTheDocument();
 });
